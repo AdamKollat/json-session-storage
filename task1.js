82d70cffac1471771ff2b2651364003c3bbbd48a
Adam <adam.kollat@gmail.com>
2022-12-12 19:41:57 +0000

    added paragraph display to js file

--- task1.js
+++ task1.js
@@ -1,4 +1,4 @@
-let container = document.getElementById('container')
+let shelf = document.getElementById('bookShelf')
 let books = []
 
 function myLoad() {
@@ -6,20 +6,15 @@ function myLoad() {
         sessionStorage.setItem("books", JSON.stringify(books));
         sessionStorage.setItem("hasCodeRunBefore", true);
     } else {
-        books = JSON.parse(sessionStorage.getItem("books"));//Get the array of person objects from sessionStorage and assign it to the array 'pers'
+        books = JSON.parse(sessionStorage.getItem("books"));//Get the array of book objects from sessionStorage and assign it to the array 'books'
         let i = 0;
-        books.forEach(function(b) {//Loop through each person (p) in the pers array
-            /*For each person in the array create an option element that displays 
+        books.forEach(function(b) {//Loop through each book (b) in the books array
+            /*For each book in the array create an option element that displays 
             that person's name and add it to the select (dropdown) element on the HTML page */
-            let optItem = document.createElement("option");
-            optItem.innerHTML = b.title;
-            optItem.value = i;
-            i = i + 1;
-            htmlSelect.appendChild(optItem);
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = `Title: ${b.title} | author: ${b.author} | genre: ${b.genre} | rating: ${b.rating}`
+            shelf.appendChild(paragraph);
         });
-        if (i > 0) {//Only make the select element visible once there is at least one person object added that the user can select.
-            htmlSelect.style.visibility = "visible";
-        }
     }
 }
 
