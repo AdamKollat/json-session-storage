50f55c18850ce68b0023dc9bc84003761a3fa05f
Adam <adam.kollat@gmail.com>
2022-12-12 19:17:50 +0000

    Added sessionstorage declaration and constructor

--- task1.js
+++ task1.js
@@ -1,8 +1,48 @@
 let container = document.getElementById('container')
+let books = []
+
+function myLoad() {
+    let htmlSelect = document.getElementById("personList");
+    htmlSelect.style.visibility = "hidden";
+
+    if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+       // let arrayOfPersonObjects = [];
+        sessionStorage.setItem("books", JSON.stringify(books));
+        sessionStorage.setItem("hasCodeRunBefore", true);
+    } else {
+        pers = JSON.parse(sessionStorage.getItem("books"));//Get the array of person objects from sessionStorage and assign it to the array 'pers'
+        let i = 0;
+        books.forEach(function(b) {//Loop through each person (p) in the pers array
+            /*For each person in the array create an option element that displays 
+            that person's name and add it to the select (dropdown) element on the HTML page */
+            let optItem = document.createElement("option");
+            optItem.innerHTML = b.title;
+            optItem.value = i;
+            i = i + 1;
+            htmlSelect.appendChild(optItem);
+        });
+        if (i > 0) {//Only make the select element visible once there is at least one person object added that the user can select.
+            htmlSelect.style.visibility = "visible";
+        }
+    }
+}
+
+function Book(title, author, genre, rating) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.rating = rating;
+}
+
 
 const newBook = () => {
-    let title = prompt('Enter title:')
-    let author = prompt('Enter author:')
-    let genre = prompt('Enter genre:')
-    let rating = prompt('Enter rating(1-5):')
+    books = JSON.parse(sessionStorage.getItem('books'))
+    let newBook = new Book(
+        document.getElementById("title").value,
+        document.getElementById("author").value,
+        document.getElementById("genre").value,
+        document.getElementById("rating").value,
+    );
+    books.push(newBook);
+    sessionStorage.setItem('books', JSON.stringify(books));
 }
