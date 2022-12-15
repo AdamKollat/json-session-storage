60cc1d2ad22157f4cb22e697b1b31d638176225a
Adam <adam.kollat@gmail.com>
2022-12-15 20:45:37 +0000

    Final update, task complete

--- task1.js
+++ task1.js
@@ -9,8 +9,9 @@ function myLoad() {
     } else {
         books = JSON.parse(sessionStorage.getItem("books")); //Get the array of book objects from sessionStorage and assign it to the array 'books'
         books.forEach(function (b) {
+           
             let paragraph = document.createElement("p");
-            paragraph.innerHTML = `Title: ${b.title} | author: ${b.author} | genre: ${b.genre} | rating: ${b.rating}`
+            paragraph.innerHTML = `Title: ${b.title} | Author: ${b.author} | Genre: ${b.genre} | Rating: ${b.rating}`
             paragraph.id = n;
             n++;
             paragraph.classList.add('bookObj');
@@ -39,6 +40,8 @@ const newBook = () => {
 }
 
 document.getElementById('bookShelf').addEventListener('click', e => {
+    let edit = prompt('Would you like to\nEdit - E\nDelete - D?')
+    if (edit === 'e' || edit === 'E') {
     let choice = prompt('Please choose what would you like to edit:\nTitle - T\nAuthor - A\nGenre - G\nRating - R');
     if (choice === 't' || choice === 'T') {
         let targetID = e.target.id;
@@ -82,5 +85,45 @@ document.getElementById('bookShelf').addEventListener('click', e => {
         document.getElementById(targetID).innerHTML = replace;
     } else {
         alert('Invalid selection. Please try again.')
-    }  
+    }
+} else if (edit === 'd' || edit === 'D') {
+    let choice = prompt('Please choose what would you like to delete:\nTitle - T\nAuthor - A\nGenre - G\nRating - R');
+    if (choice === 't' || choice === 'T') {
+        let targetID = e.target.id;
+        let currentTitle = books[targetID].title;
+        let replace = document.getElementById(targetID).innerHTML.replace(`Title: ${currentTitle} | `, '');
+        document.getElementById(targetID).innerHTML = replace;
+        delete currentTitle;
+        let sessionShelf = JSON.parse(sessionStorage.getItem('books'));
+        delete sessionShelf[targetID].title;
+        sessionStorage.setItem('books', JSON.stringify(sessionShelf))       
+} else if (choice === 'a' || choice === 'A') {
+    let targetID = e.target.id;
+    let currentAuthor = books[targetID].author;
+    let replace = document.getElementById(targetID).innerHTML.replace(`Author: ${currentAuthor} | `, '');
+    document.getElementById(targetID).innerHTML = replace;
+    delete currentAuthor;
+    let sessionShelf = JSON.parse(sessionStorage.getItem('books'));
+    delete sessionShelf[targetID].author;
+    sessionStorage.setItem('books', JSON.stringify(sessionShelf))       
+} else if (choice === 'g' || choice === 'G') {
+    let targetID = e.target.id;
+    let currentGenre = books[targetID].genre;
+    let replace = document.getElementById(targetID).innerHTML.replace(`Genre: ${currentGenre} | `, '');
+    document.getElementById(targetID).innerHTML = replace;
+    delete currentGenre;
+    let sessionShelf = JSON.parse(sessionStorage.getItem('books'));
+    delete sessionShelf[targetID].genre;
+    sessionStorage.setItem('books', JSON.stringify(sessionShelf))     
+} else if (choice === 'r' || choice === 'R') {
+    let targetID = e.target.id;
+    let currentRating = books[targetID].genre;
+    let replace = document.getElementById(targetID).innerHTML.replace(`Rating: ${currentRating} | `, '');
+    document.getElementById(targetID).innerHTML = replace;
+    delete currentRating;
+    let sessionShelf = JSON.parse(sessionStorage.getItem('books'));
+    delete sessionShelf[targetID].rating;
+    sessionStorage.setItem('books', JSON.stringify(sessionShelf))     
+} 
+}
 })
